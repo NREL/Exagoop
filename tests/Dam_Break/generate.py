22489c8493054a0e779bed07dbc493d18dd4a3ec
SreejithNREL <98907926+SreejithNREL@users.noreply.github.com>
2022-06-24 11:31:36 -0600

    updated geometry values to match Martic & Moyce test case

--- tests/Dam_Break/generate.py
+++ tests/Dam_Break/generate.py
@@ -3,19 +3,19 @@ from sys import argv
 
 # Use the same parameters as in input file
 blo    = np.array([float(0.0),float(0.0),float(0.0)])
-bhi    = np.array([float(3.0),float(3.0),float(0.09375)])
-ncells = np.array([64,64,2])
+bhi    = np.array([float(0.4),float(0.4),float(0.012)])
+ncells = np.array([100,100,3])
 npart  = 0 
 dx = (bhi-blo)/ncells;
 if(dx[0]!=dx[1] or dx[0]!=dx[2] or dx[1]!=dx[2]):
     print("Error! mesh sizes are not same in all directions")
 nparticle_per_cells_eachdir=1
-xmin=1.0
-xmax=2.0
-ymin=2.0
-ymax=2.5
+xmin=0.0
+xmax=0.1
+ymin=0.0
+ymax=0.2
 zmin=0
-zmax=0.09375
+zmax=0.012
 
 print(range(nparticle_per_cells_eachdir))
 
@@ -41,8 +41,7 @@ rad=0.025
 K_BM=2e4
 Gama_Pressure=7.0
 Dyn_visc=0.001
-E = 78200000000
-nu=0.3
+
 #Volume in each cell
 vol_cell=dx[0]*dx[1]*dx[2]
 vol_particle=vol_cell/(nparticle_per_cells_eachdir*nparticle_per_cells_eachdir*nparticle_per_cells_eachdir)
@@ -70,7 +69,5 @@ for k in range(ncells[2]):
                             outfile.write("%e\t%e\t"%(rad,dens));
                             outfile.write("%e\t%e\t%e\t"%(velx,vely,velz));
                             outfile.write("%d\t%e\t%e\t%e\n"%(1,K_BM,Gama_Pressure,Dyn_visc));
-                            #outfile.write("%d\t%e\t%e\n"%(0,E,nu));
-
 
 outfile.close()
