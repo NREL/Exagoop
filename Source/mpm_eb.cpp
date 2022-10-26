0212485493af856a977683ac2f74c9f8e365e3eb
Hariswaran Sitaraman <hsitaram@el3.ib0.cm.hpc.nrel.gov>
2022-10-26 11:05:32 -0600

    some indent edits

--- Source/mpm_eb.cpp
+++ Source/mpm_eb.cpp
@@ -10,21 +10,21 @@ namespace mpm_ebtools
     int ls_refinement=1;
     bool using_levelset_geometry=false;
     const int max_blocks=16;
-    
+
     void make_blocks_levelset(const Geometry &geom,const BoxArray &ba,const DistributionMapping &dm)
     {
         int ls_ref = ls_refinement;
         // Define nGrow of level-set and EB
         int nghost = 1;
-        
+
         const auto plo = geom.ProbLoArray();
         const auto phi = geom.ProbHiArray();
-        
+
         Array<Array<amrex::Real,3>,max_blocks> lower_corners;
         Array<Array<amrex::Real,3>,max_blocks> upper_corners;
 
         amrex::Real len_default = std::max(phi[0]-plo[0],
-                                 std::max(phi[1]-plo[1],phi[2]-plo[2]));;
+                                           std::max(phi[1]-plo[1],phi[2]-plo[2]));
 
         Vector<amrex::Real> centervec(3);
         amrex::Real lenfac=20.0;
@@ -37,49 +37,49 @@ namespace mpm_ebtools
             lower_corners[i]={centervec[0]-len_default, centervec[1]-len_default, centervec[2]-len_default};
             upper_corners[i]={centervec[0]+len_default, centervec[1]+len_default, centervec[2]+len_default};
         }
-        
+
         int nblocks;
         bool inside=false;
         Vector<amrex::Real> cvec(6);
-        
+
         amrex::ParmParse pp("blocks");
         pp.query("num_blocks",nblocks);
-        
+
         for(int i=0;i<nblocks;i++)
         {
             amrex::Print()<<"reading block "<<i<<"\n";
-        
+
             std::string corners_str=amrex::Concatenate("corners_",i,2); 
-            
+
             pp.getarr(corners_str.c_str(),    cvec,  0, 6);
             lower_corners[i]={cvec[0], cvec[1], cvec[2]};
             upper_corners[i]={cvec[3], cvec[4], cvec[5]};
         }
 
         EB2::BoxIF blocks[]={EB2::BoxIF(lower_corners[0], upper_corners[0], inside),
-                             EB2::BoxIF(lower_corners[1], upper_corners[1], inside),
-                             EB2::BoxIF(lower_corners[2], upper_corners[2], inside),
-                             EB2::BoxIF(lower_corners[3], upper_corners[3], inside),
-                             EB2::BoxIF(lower_corners[4], upper_corners[4], inside),
-                             EB2::BoxIF(lower_corners[5], upper_corners[5], inside),
-                             EB2::BoxIF(lower_corners[6], upper_corners[6], inside),
-                             EB2::BoxIF(lower_corners[7], upper_corners[7], inside),
-                             EB2::BoxIF(lower_corners[8], upper_corners[8], inside),
-                             EB2::BoxIF(lower_corners[9], upper_corners[9], inside),
-                             EB2::BoxIF(lower_corners[10], upper_corners[10], inside),
-                             EB2::BoxIF(lower_corners[11], upper_corners[11], inside),
-                             EB2::BoxIF(lower_corners[12], upper_corners[12], inside),
-                             EB2::BoxIF(lower_corners[13], upper_corners[13], inside),
-                             EB2::BoxIF(lower_corners[14], upper_corners[14], inside),
-                             EB2::BoxIF(lower_corners[15], upper_corners[15], inside)};
+            EB2::BoxIF(lower_corners[1], upper_corners[1], inside),
+            EB2::BoxIF(lower_corners[2], upper_corners[2], inside),
+            EB2::BoxIF(lower_corners[3], upper_corners[3], inside),
+            EB2::BoxIF(lower_corners[4], upper_corners[4], inside),
+            EB2::BoxIF(lower_corners[5], upper_corners[5], inside),
+            EB2::BoxIF(lower_corners[6], upper_corners[6], inside),
+            EB2::BoxIF(lower_corners[7], upper_corners[7], inside),
+            EB2::BoxIF(lower_corners[8], upper_corners[8], inside),
+            EB2::BoxIF(lower_corners[9], upper_corners[9], inside),
+            EB2::BoxIF(lower_corners[10], upper_corners[10], inside),
+            EB2::BoxIF(lower_corners[11], upper_corners[11], inside),
+            EB2::BoxIF(lower_corners[12], upper_corners[12], inside),
+            EB2::BoxIF(lower_corners[13], upper_corners[13], inside),
+            EB2::BoxIF(lower_corners[14], upper_corners[14], inside),
+            EB2::BoxIF(lower_corners[15], upper_corners[15], inside)};
 
 
         auto blocks_union = EB2::makeUnion(blocks[0],blocks[1],blocks[2],blocks[3],
-                                       blocks[4],blocks[5],blocks[6],blocks[7],
-                                       blocks[8],blocks[9],blocks[10],blocks[11],
-                                       blocks[12],blocks[13],blocks[14],blocks[15]);
+                                           blocks[4],blocks[5],blocks[6],blocks[7],
+                                           blocks[8],blocks[9],blocks[10],blocks[11],
+                                           blocks[12],blocks[13],blocks[14],blocks[15]);
 
-        
+
         //Define EB
         auto blocks_gshop = EB2::makeShop(blocks_union);
 
@@ -119,13 +119,13 @@ namespace mpm_ebtools
         amrex::FillSignedDistance (*lsphi,lslev,*ebfactory,ls_ref);
 
     }
-    
+
     void make_wedge_hopper_levelset(const Geometry &geom,const BoxArray &ba,const DistributionMapping &dm)
     {
         int ls_ref = ls_refinement;
         // Define nGrow of level-set and EB
         int nghost = 1;
-        
+
         const auto plo = geom.ProbLoArray();
         const auto phi = geom.ProbHiArray();
 
@@ -140,14 +140,14 @@ namespace mpm_ebtools
         pp.get("bin_size",      bin_size);
         pp.get("funnel_height",  funnel_height);
         pp.get("vertical_offset", vertoffset);
-        
+
         Array<amrex::Real,3> funnel_point1 ={0.5*exit_size,0.0,0.0};
         Array<amrex::Real,3> funnel_normal1={funnel_height,0.5*(exit_size-bin_size),0.0};
         EB2::PlaneIF funnel1(funnel_point1, funnel_normal1);
         Array<amrex::Real,3> bin_point1={0.5*bin_size,funnel_height,0.0};
         Array<amrex::Real,3> bin_normal1={1.0,0.0,0.0};
         EB2::PlaneIF bin1(bin_point1, bin_normal1);
-        
+
         Array<amrex::Real,3> funnel_point2 ={-0.5*exit_size,0.0,0.0};
         Array<amrex::Real,3> funnel_normal2={-funnel_height,0.5*(exit_size-bin_size),0.0};
         EB2::PlaneIF funnel2(funnel_point2, funnel_normal2);
@@ -157,7 +157,7 @@ namespace mpm_ebtools
 
         Array<Real,3> center={0.5*(plo[0]+phi[0]),vertoffset,0.5*(plo[2]+phi[2])};
         auto hopper_alone = EB2::translate(EB2::makeUnion(funnel1,bin1,funnel2,bin2),center);
-        
+
         amrex::Real len[AMREX_SPACEDIM]={phi[0]-plo[0],phi[1]-plo[1],phi[2]-plo[2]};
 
         RealArray lo,hi;
@@ -171,7 +171,7 @@ namespace mpm_ebtools
         EB2::BoxIF box_below(lo, hi, false); 
 
         auto hopper=EB2::makeComplement(EB2::makeUnion(EB2::makeComplement(hopper_alone),box_below));
-        
+
         //Define EB
         //auto hopper_gshop = EB2::makeShop(box_below);
         auto hopper_gshop = EB2::makeShop(hopper);
--- Source/mpm_particle_container.cpp
+++ Source/mpm_particle_container.cpp
@@ -40,48 +40,48 @@ void MPMParticleContainer::apply_constitutive_model(const amrex::Real& dt,
 
             if(p.idata(intData::phase)==0)
             {
-				amrex::Real xp[AMREX_SPACEDIM];
-				amrex::Real strainrate[NCOMP_TENSOR];
-				amrex::Real strain[NCOMP_TENSOR];
-				amrex::Real stress[NCOMP_TENSOR];
+                amrex::Real xp[AMREX_SPACEDIM];
+                amrex::Real strainrate[NCOMP_TENSOR];
+                amrex::Real strain[NCOMP_TENSOR];
+                amrex::Real stress[NCOMP_TENSOR];
 
-				for(int d=0;d<NCOMP_TENSOR;d++)
-				{
-					p.rdata(realData::strain+d) += dt*p.rdata(realData::strainrate+d);
-				}
-				//apply axial strain
-				p.rdata(realData::strain+XX) += dt*applied_strainrate;
-				p.rdata(realData::strain+YY) += dt*applied_strainrate;
-				p.rdata(realData::strain+ZZ) += dt*applied_strainrate;
+                for(int d=0;d<NCOMP_TENSOR;d++)
+                {
+                    p.rdata(realData::strain+d) += dt*p.rdata(realData::strainrate+d);
+                }
+                //apply axial strain
+                p.rdata(realData::strain+XX) += dt*applied_strainrate;
+                p.rdata(realData::strain+YY) += dt*applied_strainrate;
+                p.rdata(realData::strain+ZZ) += dt*applied_strainrate;
 
-				for(int d=0;d<NCOMP_TENSOR;d++)
-				{
-					strainrate[d]=p.rdata(realData::strainrate+d);
-					strain[d]=p.rdata(realData::strain+d);
-				}
+                for(int d=0;d<NCOMP_TENSOR;d++)
+                {
+                    strainrate[d]=p.rdata(realData::strainrate+d);
+                    strain[d]=p.rdata(realData::strain+d);
+                }
 
-				if(p.idata(intData::constitutive_model)==0)		//Elastic solid
-				{
-					linear_elastic(strain,strainrate,stress,p.rdata(realData::E),p.rdata(realData::nu));
-				}
-				else if(p.idata(intData::constitutive_model)==1)		//Viscous fluid with approximate EoS
-				{
-					p.rdata(realData::pressure) = p.rdata(realData::Bulk_modulus)*
-						(pow(1/p.rdata(realData::jacobian),p.rdata(realData::Gama_pressure))-1.0)+p_inf;
-					Newtonian_Fluid(strainrate,stress,p.rdata(realData::Dynamic_viscosity),p.rdata(realData::pressure));
-				}
+                if(p.idata(intData::constitutive_model)==0)		//Elastic solid
+                {
+                    linear_elastic(strain,strainrate,stress,p.rdata(realData::E),p.rdata(realData::nu));
+                }
+                else if(p.idata(intData::constitutive_model)==1)		//Viscous fluid with approximate EoS
+                {
+                    p.rdata(realData::pressure) = p.rdata(realData::Bulk_modulus)*
+                    (pow(1/p.rdata(realData::jacobian),p.rdata(realData::Gama_pressure))-1.0)+p_inf;
+                    Newtonian_Fluid(strainrate,stress,p.rdata(realData::Dynamic_viscosity),p.rdata(realData::pressure));
+                }
 
-				for(int d=0;d<NCOMP_TENSOR;d++)
-				{
-					p.rdata(realData::stress+d)=stress[d];
-				}
+                for(int d=0;d<NCOMP_TENSOR;d++)
+                {
+                    p.rdata(realData::stress+d)=stress[d];
+                }
             }
         });
     }
 } 
 
 void MPMParticleContainer::apply_constitutive_model_delta(const amrex::Real& dt,
-                                                    amrex::Real applied_strainrate=0.0)
+                                                          amrex::Real applied_strainrate=0.0)
 {
     const int lev = 0;
     const Geometry& geom = Geom(lev);
@@ -116,43 +116,43 @@ void MPMParticleContainer::apply_constitutive_model_delta(const amrex::Real& dt,
 
             if(p.idata(intData::phase)==0)
             {
-				amrex::Real xp[AMREX_SPACEDIM];
-				amrex::Real strainrate[NCOMP_TENSOR];
-				amrex::Real delta_strain[NCOMP_TENSOR];
-				amrex::Real delta_stress[NCOMP_TENSOR];
+                amrex::Real xp[AMREX_SPACEDIM];
+                amrex::Real strainrate[NCOMP_TENSOR];
+                amrex::Real delta_strain[NCOMP_TENSOR];
+                amrex::Real delta_stress[NCOMP_TENSOR];
 
-				for(int d=0;d<NCOMP_TENSOR;d++)
-				{
-					p.rdata(realData::strain+d) += dt*p.rdata(realData::strainrate+d);
-				}
-				//apply axial strain
-				p.rdata(realData::strain+XX) += dt*applied_strainrate;
-				p.rdata(realData::strain+YY) += dt*applied_strainrate;
-				p.rdata(realData::strain+ZZ) += dt*applied_strainrate;
+                for(int d=0;d<NCOMP_TENSOR;d++)
+                {
+                    p.rdata(realData::strain+d) += dt*p.rdata(realData::strainrate+d);
+                }
+                //apply axial strain
+                p.rdata(realData::strain+XX) += dt*applied_strainrate;
+                p.rdata(realData::strain+YY) += dt*applied_strainrate;
+                p.rdata(realData::strain+ZZ) += dt*applied_strainrate;
 
-				for(int d=0;d<NCOMP_TENSOR;d++)
-				{
-					delta_strain[d]=dt*p.rdata(realData::strainrate+d);
-				}
+                for(int d=0;d<NCOMP_TENSOR;d++)
+                {
+                    delta_strain[d]=dt*p.rdata(realData::strainrate+d);
+                }
 
-				//apply axial strain
-				delta_strain[XX] += dt*applied_strainrate;
-				delta_strain[YY] += dt*applied_strainrate;
-				delta_strain[ZZ] += dt*applied_strainrate;
+                //apply axial strain
+                delta_strain[XX] += dt*applied_strainrate;
+                delta_strain[YY] += dt*applied_strainrate;
+                delta_strain[ZZ] += dt*applied_strainrate;
 
-				if(p.idata(intData::constitutive_model)==0)		//Elastic solid
-				{
-					linear_elastic(delta_strain,delta_stress,p.rdata(realData::E),p.rdata(realData::nu));
-				}
-				else if(p.idata(intData::constitutive_model)==1)		//Viscous fluid with approximate EoS
-				{
-					amrex::Abort("\nDelta strain model for weakly compressible fluids not implemented yet.");
-				}
+                if(p.idata(intData::constitutive_model)==0)		//Elastic solid
+                {
+                    linear_elastic(delta_strain,delta_stress,p.rdata(realData::E),p.rdata(realData::nu));
+                }
+                else if(p.idata(intData::constitutive_model)==1)		//Viscous fluid with approximate EoS
+                {
+                    amrex::Abort("\nDelta strain model for weakly compressible fluids not implemented yet.");
+                }
 
-				for(int d=0;d<NCOMP_TENSOR;d++)
-				{
-					p.rdata(realData::stress+d)+=delta_stress[d];
-				}
+                for(int d=0;d<NCOMP_TENSOR;d++)
+                {
+                    p.rdata(realData::stress+d)+=delta_stress[d];
+                }
             }
         });
     }
