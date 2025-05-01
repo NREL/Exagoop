6afb7fc03da30c4822297df83f21007469e45f81
hsitaram <hariswaran@gmail.com>
2025-05-01 12:00:39 -0600

    fixing particle generation script in falling block

--- tests/falling_block/generate.py
+++ tests/falling_block/generate.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sys import argv
+import sys
 
 def transform(x,y,z,center,angle):
     xd=(x-center[0])*np.cos(angle)+(y-center[1])*np.sin(angle)+center[0]
@@ -7,13 +8,25 @@ def transform(x,y,z,center,angle):
     zd=z
     return(xd,yd,zd)
 
+if(len(argv)<14):
+    print("\n\n")
+    print("This script generates particles in a box")
+    print("****************************************")
+    print("Inputs include: box lower corner (3 coordinates), box upper corner (3 coordinates)")
+    print("box number of cells (3 ints), inital velocity (3 components), box tilt (in degrees)")
+    print("e.g. usage python generate.py 0.4 0.4 0.4 0.6 0.8 0.6 20 20 20 0 -10 0 45")
+    print("Hard coded properties: density=500 kg/m3; Youngs modulus=2e5 Pa;")
+    print("Poissons ratio:0.2; Particle radius=0.025 m")
+    print("\n\n")
+    sys.exit()
+
 blo    = np.array([float(argv[1]),float(argv[2]),float(argv[3])])
 bhi    = np.array([float(argv[4]),float(argv[5]),float(argv[6])])
 ncells = np.array([int(argv[7]),int(argv[8]),int(argv[9])])
+angle=float(argv[13])*np.pi/180.0
 npart  = ncells[0]*ncells[1]*ncells[2];
 center=0.5*(blo+bhi)
 dx = (bhi-blo)/ncells;
-print(dx)
 
 outfile=open("mpm_particles.dat","w")
 outfile.write("%d\n"%(npart));
@@ -21,7 +34,9 @@ outfile.write("%d\n"%(npart));
 dens=500
 rad=0.025
 phase=0
-angle=np.pi/4
+constmodel=0
+Ymod=2e5
+pratio=0.2
 for k in range(ncells[2]):
     for j in range(ncells[1]):
         for i in range(ncells[0]):
@@ -32,13 +47,14 @@ for k in range(ncells[2]):
 
             part_cx,part_cy,part_cz=transform(cell_cx,cell_cy,cell_cz,center,angle)
 
-            velx=0.0;
-            vely=-5.0;
-            velz=0.0;
+            velx=float(argv[10]);
+            vely=float(argv[11]);
+            velz=float(argv[12]);
     
             outfile.write("%d\t%e\t%e\t%e\t"%(phase,part_cx,part_cy,part_cz));
             outfile.write("%e\t%e\t"%(rad,dens));
-            outfile.write("%e\t%e\t%e\n"%(velx,vely,velz));
+            outfile.write("%e\t%e\t%e\t"%(velx,vely,velz));
+            outfile.write("%d\t%e\t%e\n"%(constmodel,Ymod,pratio));
 
 
 outfile.close()
