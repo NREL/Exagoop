2c0d96bcffd8486428ea6c5bfb94283dec1ff90b
SreejithNREL <Sreejith.NadakkalAppukuttan@nrel.gov>
2025-09-19 12:20:23 -0600

    modified cleaning script

--- build/clean_build_files.sh
+++ build/clean_build_files.sh
@@ -1,6 +1,2 @@
-rm -rf CMakeCache.txt
-rm -rf CMakeFiles
-rm -rf cmake_install.cmake
-rm -rf ExaGOOP.exe
-rm -rf Makefile
-rm -rf Submodules
+find . -mindepth 1 ! -name 'cmake.sh' ! -name 'clean_build_files.sh' -exec rm -rf {} +
+
